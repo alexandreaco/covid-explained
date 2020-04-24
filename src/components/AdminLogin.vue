1b89ab601c572d35cbab326e57d36d26817476f6
Liv Marx <oehrenmarks@gmail.com>
2020-04-24 13:04:49 -0400

    Create admin login view.

---
+++ src/components/AdminLogin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="admin-login">
+    <br />
+    <br />
+    <br />
+    <h3>Admin Login Coming Soon!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminLogin',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
