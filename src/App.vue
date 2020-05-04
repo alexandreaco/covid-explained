1a8c251f4799397d8f1d22b23fde852bdf573166
Matt <matt@motel.is>
2020-05-04 19:04:19 -0400

    add app styles

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,65 @@ export default {
 </script>
 
 <style>
+body{
+  font-family: 'Libre Franklin', sans-serif;
+  background:#F4F3E9;
+  color:#33322D;
+  margin:120px 100px 100px 0;
+}
+h1,h2,h3,h4,.card-title{
+  font-family: 'Playfair Display', serif;
+}
+h3 {
+  margin: 0px;
+  font-size:96px;
+  line-height:100px;
+}
+
+
+.post-card {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  padding:1rem;
+  margin:0 1rem 1rem 0;
+  transform: rotate(-1deg);
+  transition:.5s all;
+}
+.post-card:hover {
+    transform: rotate(.5deg);
+}
+
+
+.questions{
+  background:#CF5C36;
+  color:#FDFFFF;
+}
+.news{
+  background:#C7D3DD;
+}
+.definitions{
+  background:#736372;
+  color:#FDFFFF;
+}
+
+.explainers{
+  background:#197278;
+  color:#FDFFFF;
+}
+.scenarios{
+  background:#283D3B;
+  color:#FDFFFF;
+}
+
+.search-bar{
+  position: fixed;
+  z-index:5;
+  left:200px;
+  top:20px;
+}
+.search-bar input{
+  padding:1rem;
+  border:1px solid gray;
+}
 </style>
