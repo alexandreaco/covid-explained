9aedef3c745f575b2fcbf41c3141d688c53f3daf
Matt <matt@motel.is>
2020-05-05 15:59:57 -0400

    formatting App

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,125 @@
 
 <template>
+
   <div id="app">
     <NavBar />
     <SideNav />
     <router-view />
   </div>
+
 </template>
 
 <script>
-import NavBar from '@/components/NavBar';
-import SideNav from '@/components/Sidenav';
-export default {
-  name: 'App',
-  components: {
-    NavBar,
-    SideNav,
-  },
-};
+
+  import NavBar from '@/components/NavBar'
+  import SideNav from '@/components/Sidenav'
+  export default {
+    name: 'App',
+    components: {
+      NavBar,
+      SideNav
+    }
+  }
+
 </script>
 
 <style>
-body{
-  font-family: 'Libre Franklin', sans-serif;
-  background:#F4F3E9;
-  color:#33322D;
-  margin:120px 130px 100px 0;
-}
-h1,h2,h3,h4,.card-title{
-  font-family: 'Playfair Display', serif;
-}
-h3 {
-  margin: 0px;
-  font-size:96px;
-  line-height:100px;
-}
 
+  body {
+    font-family: 'Libre Franklin', sans-serif;
+    background: #f4f3e9;
+    color: #33322d;
+    margin: 120px 130px 100px 0;
+  }
+  @media (max-width: 1024px){
+    body {
+        margin: 120px 0px 100px 0;
+    }
 
-.post-card {
-  position: relative;
-  width:33%;
-  padding:1rem;
-  margin:.15%;
-  /* transform: rotate(-1deg); */
-  transition:.5s all;
-}
-.post-card:hover {
-    transform: rotate(.5deg);
-}
+  }
 
-.bullet{
-  display: inline-block;
-  width:10px;
-  height:10px;
-}
+  h1,
+  h2,
+  h3,
+  h4,
+  .card-title {
+    font-family: 'Playfair Display', serif;
+  }
+  h3 {
+    margin: 0px;
+    font-size: 96px;
+    line-height: 100px;
+  }
 
+  .post-card {
+    position: relative;
+    width: 33%;
+    padding: 1rem;
+    margin: 0.15%;
+    /* transform: rotate(-1deg); */
+    transition: 0.5s all;
+  }
+  .post-card:hover {
+    transform: rotate(0.5deg);
+  }
 
-.questions{
-  background:#DD694A;
-  color:#F8E1DB;
-}
-.news{
-  background:#33322D;
-  color:#F4F3E9;
-}
-.definitions{
-  background:#604E60;
-  color:#FDFFFF;
-}
-.explainers{
-  background:#197278;
-  color:#FDFFFF;
-}
-.scenarios{
-  background:#C7D3DD;
-  /* color:#FDFFFF; */
-}
+  .bullet {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+  }
 
-.post-card.definitions{
-  word-wrap: break-word;
-  background: #D9BDD7;
-  color:#604E60;
-}
-.post-card.explainers{
-  color: #D1E3E4;
-}
-.post-card.news{
-  border: 1px solid #33322D;
-  color:#33322D;
-  background:#F4F3E9;
-}
-.text-snippet{
-  font-size:12px;
-}
-/*
-.post-card.questions .card-title{
-  font-family: 'Libre Franklin', sans-serif;
-  font-style: italic;
-}
-.definitions a .card-title {
-    font-size: 48px;
-    line-height: 48px;
-    font-weight:100;
-}
-.definitions .text-author,.definitions .text-snippet,
-.explainers .text-author,.explainers .text-snippet{
-  display: none;
-}
-*/
+  .questions {
+    background: #dd694a;
+    color: #f8e1db;
+  }
+  .news {
+    background: #33322d;
+    color: #f4f3e9;
+  }
+  .definitions {
+    background: #604e60;
+    color: #fdffff;
+  }
+  .explainers {
+    background: #197278;
+    color: #fdffff;
+  }
+  .scenarios {
+    background: #c7d3dd;
+  }
 
-.search-bar{
-  position: fixed;
-  z-index:5;
-  left:200px;
-  top:20px;
-}
-.search-bar input{
-  padding:1rem;
-  border:1px solid gray;
-}
+  .post-card.definitions {
+    word-wrap: break-word;
+    background: #d9bdd7;
+    color: #604e60;
+  }
+  .post-card.definitions .text-author,
+  .post-card.definitions .text-snippet {
+  }
+  .post-card.explainers {
+    color: #d1e3e4;
+  }
+  .post-card.news {
+    border-bottom: 1px solid #33322d;
+    padding: 1rem 0;
+    color: #33322d;
+    background: #f4f3e9;
+    width: 100%;
+  }
+  .text-snippet {
+    font-size: 12px;
+  }
+
+  .search-bar {
+    position: fixed;
+    z-index: 5;
+    left: 200px;
+    top: 20px;
+  }
+  .search-bar input {
+    padding: 1rem;
+    border: 1px solid gray;
+  }
+
 </style>
